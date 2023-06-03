4b6af6b39ffe605bee814c180776935bb51956ec
VSaverskyi <vovikck15@gmail.com>
2023-06-03 15:14:26 +0200

    add empty pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import Cast from "./pages/Cast";
+import Reviews from "./pages/Reviews";
+
+// const API_KEY = 'd5b6a5ca94641e9f7b547d0396725c93';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/movies' element={<Movies />} >
+        <Route path=":movieId" element={<MovieDetails />} >
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
+
+export default App;
